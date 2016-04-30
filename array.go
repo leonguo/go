292e00f133d389ba35a60b7780995655b632575c
leon <hotman8168@gmail.com>
2016-04-30 23:25:01 +0800

    for ifelse switch

---
+++ array.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printArrayValue(arr [1]int) {
+	fmt.Println("arr[0] is ", &arr[0])
+}
+
+func printAarrayref(arr *[1]int) {
+	fmt.Println("arr[0] address", &arr[0])
+}
+
+func main() {
+	v_IntArray := [...]int{}
+	fmt.Printf("%T\n", v_IntArray)
+	fmt.Println("len arr is", len(v_IntArray))
+	fmt.Println("cap arr is", cap(v_IntArray))
+
+	v_IntArrayOf5 := [5]int{}
+	fmt.Println("len(v_IntArrayOf5)", len(v_IntArrayOf5))
+	fmt.Println("v_IntArrayOf5", v_IntArrayOf5)
+
+	v_IntArrayOf5[2] = 3
+	fmt.Println("v_IntArrayOf5", v_IntArrayOf5)
+
+	v_IntArrayOf5 = [5]int{1, 2}
+	fmt.Println("len arr:", len(v_IntArrayOf5))
+	fmt.Println("v_IntArrayOf5", v_IntArrayOf5)
+
+	v_IntArrayOf5 = [5]int{3, 1, 3: 4, 10}
+	fmt.Println("len(v_IntArrayOf5: ", len(v_IntArrayOf5))
+	fmt.Println("v_IntArrayOf5: ", v_IntArrayOf5)
+
+	v_IntArrayOf1 := [1]int{}
+	fmt.Println("address arr[0]", &v_IntArrayOf1[0])
+	fmt.Println(v_IntArrayOf1)
+
+	printArrayValue(v_IntArrayOf1)
+	printAarrayref(&v_IntArrayOf1)
+
+	v_IntArrayof23 := [2][3]int{}
+	fmt.Println(v_IntArrayof23)
+
+	v_IntArray1 := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	v_IntSlice := v_IntArray1[1:4:9]
+	fmt.Println("v_IntArray[1:4:9]", v_IntSlice)
+	fmt.Println("v_IntArray[1:4:9] len ", len(v_IntSlice))
+}
