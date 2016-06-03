fca0d855b8afa739f4f545058a72f75b94f930cb
leon <hotman8168@gmail.com>
2016-06-03 23:50:51 +0800

    时间

--- epoch.go
+++ epoch.go
@@ -20,4 +20,14 @@ func main() {
 	fmt.Println(time.Unix(secs, 0))
 	fmt.Println(time.Unix(0, nanos))
 
+	ticker := time.NewTicker(time.Millisecond * 1000)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("tick at ", t)
+		}
+	}()
+
+	time.Sleep(time.Millisecond * 5600)
+	ticker.Stop()
+	fmt.Println("ticker stopped")
 }
