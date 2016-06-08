bff1bcdee9e9d66f539101df3025a3a6cf0bd641
leon <hotman8168@gmail.com>
2016-06-08 23:33:29 +0800

    时间

--- epoch.go
+++ epoch.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -30,4 +31,17 @@ func main() {
 	time.Sleep(time.Millisecond * 5600)
 	ticker.Stop()
 	fmt.Println("ticker stopped")
+
+	fmt.Println(rand.Intn(100), ',')
+	fmt.Println(rand.Intn(100))
+	fmt.Println()
+	fmt.Println(rand.Float64())
+
+	fmt.Println((rand.Float64()*5)+5, ',')
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	fmt.Print(r1.Intn(100), ',')
+
 }
