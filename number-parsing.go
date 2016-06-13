35b5056bafa9941ed470e5584613cc5fff96bb57
guoxingjun <hotman8168@gmail.com>
2016-06-13 18:37:01 +0800

    url

---
+++ number-parsing.go
@@ -0,0 +1,20 @@
+package main
+
+import "strconv"
+import "fmt"
+
+func main() {
+
+	f,_ := strconv.ParseFloat("1.234",64)
+	fmt.Println(f)
+
+	i,_ := strconv.ParseInt("123",0,64)
+	fmt.Println(i)
+
+	d,_ := strconv.ParseInt("0x1c8",0,64)
+	fmt.Println(d)
+
+	u,_ := strconv.ParseUint("789",0,64)
+
+	fmt.Println(u)	
+}
