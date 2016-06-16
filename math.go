12a69790de6e5a457ba0f876711baddcbb833a7b
guoxingjun <hotman8168@gmail.com>
2016-06-16 19:20:56 +0800

    循环

--- math.go
+++ math.go
@@ -6,6 +6,22 @@ func add (x,y int) int {
 	return x + y
 }
 
+func swap(x,y string) (string,string){
+	return y,x
+} 
+
+const (
+	Big = 1 << 1 // 位运算 左移动1位
+)
+
 func main(){
 	fmt.Println(add(2,56))	
+	a,b := swap("hello","world")
+	fmt.Println(a,b);
+	
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum +=  i
+	}
+	fmt.Println(sum)	
 }
