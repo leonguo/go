5f50197637246d8abeb1570cd569ea8d71202807
guoxingjun <hotman8168@gmail.com>
2016-06-26 23:06:44 +0800

    for

--- struct-literals.go
+++ struct-literals.go
@@ -1,31 +1,44 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Vertex struct {
-	X, Y int 
+	X, Y int
 }
 
 var (
-	v1 = Vertex{1,2}
-	v2 = Vertex{X:1}	
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
 	v3 = Vertex{}
-	p = &Vertex{1,2}
+	p  = &Vertex{1, 2}
 )
 
-func main(){
-	fmt.Println(v1,p,v2,v3)
-	
+func main() {
+	fmt.Println(v1, p, v2, v3)
+
 	a := make([]int, 5)
-		
+
 	fmt.Println(a)
-	
-	var b []int 
+
+	var b []int
 	fmt.Println(b)
-	b = append(b,0)
+	b = append(b, 0)
 	fmt.Println(b)
-	b = append(b,1)
+	b = append(b, 1)
 	fmt.Println(b)
-	b = append(b,2,3,4,5)
+	b = append(b, 2, 3, 4, 5)
 	fmt.Println(b)
+
+	for i := range b {
+		fmt.Println(i)
+		b[i] = 1 << uint(i)
+	}
+
+	fmt.Printf("bis %v", b)
+
+	for _, value := range b {
+		fmt.Println(value)
+	}
 }
