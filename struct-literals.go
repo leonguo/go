247859422a49592f4fb44f6070c8b5dfaab61811
guoxingjun <hotman8168@gmail.com>
2016-06-27 23:57:06 +0800

    map

--- struct-literals.go
+++ struct-literals.go
@@ -8,6 +8,19 @@ type Vertex struct {
 	X, Y int
 }
 
+type Vertex1 struct {
+	Lat, Long float64
+}
+
+var m = map[string]Vertex1{
+	"Bell Labs": Vertex1{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex1{
+		37.42202, -122.08408,
+	},
+}
+
 var (
 	v1 = Vertex{1, 2}
 	v2 = Vertex{X: 1}
@@ -41,4 +54,6 @@ func main() {
 	for _, value := range b {
 		fmt.Println(value)
 	}
+
+	fmt.Println(m)
 }
