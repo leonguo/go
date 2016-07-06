3e2f974eff7c8d06be6336128f450cb02f59e7e2
guoxingjun <hotman8168@gmail.com>
2016-07-06 23:52:44 +0800

    function

--- function-values.go
+++ function-values.go
@@ -9,6 +9,14 @@ func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
 }
 
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
 func main() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
@@ -18,4 +26,12 @@ func main() {
 	fmt.Println(compute(hypot))
 	fmt.Println(math.Pow)
 	fmt.Println(compute(math.Pow))
+
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
