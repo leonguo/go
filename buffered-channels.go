5e5e46a434d8e69979a22c23e8628519defa9e72
guoxingjun <hotman8168@gmail.com>
2016-07-13 23:48:21 +0800

    时间

--- buffered-channels.go
+++ buffered-channels.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+}
 func main() {
 	ch := make(chan int, 2)
 	ch <- 1
 	ch <- 2
 	fmt.Println(<-ch)
 	fmt.Println(<-ch)
+
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
