02787b8254dacc74b0c08677443e52c483cdcf79
guoxingjun <hotman8168@gmail.com>
2016-07-19 23:51:42 +0800

    select

--- default-selection.go
+++ default-selection.go
@@ -6,6 +6,26 @@ import (
 )
 
 func main() {
+	c4 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c4 <- "result 2"
+	}()
+	select {
+	case res := <-c4:
+		fmt.Println(res)
+	case <-time.After(time.Second * 3):
+		fmt.Println("timeout 2")
+	}
+
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+	close(queue)
+	for elem := range queue {
+		fmt.Println(elem)
+	}
+
 	tick := time.Tick(100 * time.Millisecond)
 	boom := time.After(500 * time.Millisecond)
 	for {
@@ -20,4 +40,5 @@ func main() {
 			time.Sleep(50 * time.Millisecond)
 		}
 	}
+
 }
