6bbe8c5e76b2466c9e2af48c82e6a48e98b40c89
guoxingjun <hotman8168@gmail.com>
2016-07-24 23:06:11 +0800

    特殊字符处理

--- go_test1.go
+++ go_test1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"unicode/utf8"
 )
 
@@ -14,4 +15,17 @@ func main() {
 	const str = "one world 世界大同"
 	fmt.Println(len(str))
 	fmt.Println(utf8.RuneCountInString(str))
+
+	str1 := "on 时间"
+
+	r := []rune(str1)
+	fmt.Printf("%#v\n", r)
+
+	str1 = string(r)
+	fmt.Printf("%#v\n", str1)
+
+	s := strconv.QuoteRune('☺')
+	fmt.Println(s)
+	fmt.Println('☺')
+
 }
