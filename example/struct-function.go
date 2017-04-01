4b26640593d2da94f5c2024d1c4cafcf431f0e17
wings <hotman8168@gmail.com>
2017-04-01 11:05:59 +0800

    doc

--- example/struct-function.go
+++ example/struct-function.go
@@ -6,6 +6,18 @@ type Circle struct {
 	radius float64
 }
 
+type User struct {
+	Name string
+	Age  int
+}
+
+func NewUser(name string, age int) *User {
+	return &User{
+		Name: name,
+		Age:  age,
+	}
+}
+
 func main() {
 	var c Circle
 	var d = 10
@@ -17,6 +29,9 @@ func main() {
 	fmt.Println("area is :", c.getArea())
 	fmt.Println("area is :", c.radius)
 	fmt.Println("area is :", &c)
+
+	user := NewUser("guoxingjun", 20)
+	fmt.Println(user.Name)
 }
 
 func (c *Circle) getArea() float64 {
