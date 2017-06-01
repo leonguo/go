3b2aeddd5d5e49beccdba614b6b8de1992cb416b
wings <hotman8168@gmail.com>
2017-06-01 10:11:24 +0800

    go doc

--- server/mutex.go
+++ server/mutex.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"sync"
+	"fmt"
+)
+
 /**
 	mutex 互斥锁
 	互斥锁是传统并发程序对共享资源进行访问的主要手段。是sync包中的Mutex结构体
@@ -9,3 +14,40 @@ package main
 	   func (m *Mutex) UnLock() {}
 	我们来通过一个简单的例子来说明他的用法
  */
+var c chan int
+
+type safeCounter struct {
+	number int
+	sync.Mutex
+}
+
+func (sc *safeCounter) Increment(i int) {
+	sc.Lock()
+	sc.number++
+	sc.Unlock()
+	c <- i
+}
+func (sc *safeCounter) Decrement(i int) {
+	sc.Lock()
+	sc.number--
+	sc.Unlock()
+	c <- i
+}
+func (sc *safeCounter) getNumber() int {
+	sc.Lock()
+	number := sc.number
+	sc.Unlock()
+	return number
+}
+func main() {
+	sc := new(safeCounter)
+	//w := sync.WaitGroup{}
+	c = make(chan int)
+	for i := 0; i < 10; i++ {
+		go sc.Increment(i)
+		fmt.Printf("%v", <-c)
+		go sc.Decrement(i)
+		fmt.Printf("%v", <-c)
+	}
+	fmt.Println(sc.getNumber())
+}
