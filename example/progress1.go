c69ffe29fc13b83a24a1af79055fa5ba98433a2d
wings <hotman8168@gmail.com>
2017-10-27 00:50:42 +0800

    test

--- example/progress1.go
+++ example/progress1.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bytes"
+	"strconv"
+	"fmt"
 )
 
+const STEP, NUM = 4, 40
+
 var (
-	chans     [4]chan int      //创建生产者
-	files     [4]*bytes.Buffer //创建接受者
+	chans     [STEP]chan int      //创建生产者
+	files     [STEP]*bytes.Buffer //创建接受者 一个缓存byte类型的缓冲器存放着都是byte
 	last, cur = 0, 0
 )
 
@@ -18,7 +22,15 @@ func main() {
 			for {
 				out <- prime
 			}
-		}(chans[i],i)
+		}(chans[i], i)
 	}
-
+	for j := 0; j < NUM; j++ {
+		k := <-chans[j%STEP] + 1
+		files[cur].WriteString(strconv.Itoa(k))
+		// 当前游标如果到达最大值 则停留一次，然后继续前进
+		if k != STEP || cur == last {
+			last, cur = cur, (cur+1)%4
+		}
+	}
+	fmt.Print(files)
 }
